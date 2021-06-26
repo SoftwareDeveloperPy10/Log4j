c9455ca9492f29f8f5e046a2aca7c20556410b2a
Mirac Yuksel <udemymirac23@gmail.com>
2021-06-26 06:40:04 +0300

    FileAppender

--- src/main/java/App/TestAppl.java
+++ src/main/java/App/TestAppl.java
@@ -1,10 +1,14 @@
 package App;
 
+import java.io.IOException;
+
 import org.apache.log4j.Appender;
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.HTMLLayout;
 import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.xml.XMLLayout;
 
@@ -13,11 +17,12 @@ public class TestAppl {
 	//every class has to be one logger object
 	private static Logger log = Logger.getLogger(TestAppl.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//1. Create Layout
-		Layout layout = new XMLLayout();
+		Layout layout = new PatternLayout("%p %d %C %M %m %n");
 		//2. Create Appender + link Layout
-		Appender app = new ConsoleAppender(layout);
+		//Appender app = new ConsoleAppender(layout);
+		Appender app = new FileAppender(layout,"C:\\wamp64\\www\\Log4JTutorial\\logs\\data.log");
 		//3 Link appender with logger
 		log.addAppender(app);
 		
